13c6f2ec14ebad0be51a9495af41793a2cae5363
Maher Hasan <145195694+Maher-Hasan@users.noreply.github.com>
2024-04-06 17:47:32 +0300

    solved js-ex4 (used for-of loop)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,12 @@
-const removeFromArray = function (array, ...elementsToRemove) {
-    for (let element of elementsToRemove) {
-        let index;
-        while ((index = array.indexOf(element)) !== -1) {
-            array.splice(index, 1);
+const removeFromArray = function (array, ...elementsToRemove) { //the ...elementsToRemove is used to receive an array of inputs/values.
+    for (let element of elementsToRemove) {// cycle through each element alone.
+        let index; // declare index to save indexes of the element fetched,found in the array.
+        while ((index = array.indexOf(element)) !== -1) {// loop through 'array' and saving each index of the 'element' in the index.
+            array.splice(index, 1); //remove from array 1 element where index = index.
         }
     }
     return array;
 };
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
