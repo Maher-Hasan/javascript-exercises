63606c69ca9b64dc4e0ed322507fa9ae1166eec3
Maher Hasan <145195694+Maher-Hasan@users.noreply.github.com>
2024-04-06 17:25:26 +0300

    solve js-ex4 (using for-of loop) and a while loop.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elementsToRemove) {
+    for (let element of elementsToRemove) {
+        let index;
+        while ((index = array.indexOf(element)) !== -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
