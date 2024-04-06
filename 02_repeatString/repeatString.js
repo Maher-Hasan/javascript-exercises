ec69676c2bbcd256f1e2426ec84b6e098381b952
Maher Hasan <145195694+Maher-Hasan@users.noreply.github.com>
2024-04-06 16:41:39 +0300

    Solved 2 exercise (learned how to run tests). Had to make somechanges for test 5 other test passed successfully.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, numTimes) {
+    let repeatedString = ''; // initialize variable of the result to have an empty string
+    if (numTimes < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < numTimes; i++) { // loop for numTimes
+            repeatedString += str; //add the given string each loop for the result variable
+        }
+        return repeatedString; // return result
+    }
 };
 
 // Do not edit below this line
