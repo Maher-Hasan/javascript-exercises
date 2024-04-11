a9f0381a1076b79ec6f2385f0095ecde66bc8ad9
Maher Hasan <145195694+Maher-Hasan@users.noreply.github.com>
2024-04-11 20:50:42 +0300

    ChatGPT helped me with this one :p

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Reverse the string
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    
+    // Check if the original and reversed strings are the same
+    return cleanedStr === reversedStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
