4fcacecd66942dad70afad3dcb8df8faef9ed5ad
Maher Hasan <145195694+Maher-Hasan@users.noreply.github.com>
2024-04-11 21:35:26 +0300

    fibonacci function done :)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else if (n == 2) {
+        return 1;
+    }
 
-};
+    let a = 1;
+    let b = 1;
+    let result = 0;
+    for (let i = 3; i <= n; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
