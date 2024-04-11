c17ac120c7ebf83a0ca134c2ea4ce202a196630d
Maher Hasan <145195694+Maher-Hasan@users.noreply.github.com>
2024-04-11 22:03:03 +0300

    Using the .forEach function to navigate through objects, getting the age and comparing it with previous maxAge.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = null;
+    let maxAge = -1;
 
+    people.forEach(person => {
+        const currentAge = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+        if (currentAge > maxAge) {
+            maxAge = currentAge;
+            oldestPerson = person;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
